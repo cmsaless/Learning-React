33011106bb0bbe03b788f00b0f7bfdaedc365bd7
Chris Saless <cmsaless@buffalo.edu>
2019-07-08 18:08:32 -0400

    learning to use JSON with React

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import Person from './Person/Person.js'
 
 class App extends Component {
 
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'John', age: 13 }
+        ]
+    }
+
+
+    switchNameHandler = () => {
+        console.log('Was clicked!');
+    }
+
     render() {
 
         var div = 
@@ -16,7 +28,9 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <Person />
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Running</Person>
             </div>
 
         return div
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <h1>I'm a person!</h1>
-}
+const person = (props) => {
+    return (
+        <div>
+            <h1>I'm {props.name}! I am {props.age} years</h1>
+            <h2>{props.children}</h2>
+        </div>
+    );
+};
 
 export default person;
