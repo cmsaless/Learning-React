6a4ca76ea068801db6b2725d888da5227403f0e1
Chris Saless <cmsaless@buffalo.edu>
2019-08-01 18:12:38 -0400

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
@@ -104,7 +104,7 @@ class App extends Component {
 
         return (
             <StyleRoot>
-                <div className="App">
+                <div className={classes.App}>
                     <h1>Hi, I'm a React App</h1>
                     <p className={classes.join(' ')}>This is really working!</p>
                     <button
